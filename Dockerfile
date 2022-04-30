68b621c1fbb42378bbe547410a161965174595ea
torriz <torriz@users.noreply.github.com>
2022-04-30 16:47:19 +0200

    add files

---
+++ Dockerfile
@@ -0,0 +1,54 @@
+FROM alpine:3.11.5
+LABEL maintainer="Tom Valk <tomvalk@lt-box.info>"
+ENV DEDICATED_URL http://files.v04.maniaplanet.com/server/ManiaplanetServer_Latest.zip
+ENV PROJECT_ROOT /app/dedicated
+ENV TEMPLATE_DIR /app/dedicated-configs
+
+# Create maniaplanet user/group
+RUN addgroup -g 1000 maniaplanet && \
+    adduser -u 1000 -D -G maniaplanet maniaplanet
+
+# Link the musl to glibc as it's compatible (required in Alpine image).
+RUN wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub \
+    && wget -q https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0/glibc-2.31-r0.apk \
+    && apk add glibc-2.31-r0.apk libstdc++ musl libuuid wget
+
+ENV LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/:/lib/"
+
+# Create project root.
+RUN mkdir -p $PROJECT_ROOT 
+RUN mkdir -p $TEMPLATE_DIR
+WORKDIR $PROJECT_ROOT
+
+# Download dedicated.
+RUN wget --no-verbose --show-progress --progress=bar:force:noscroll $DEDICATED_URL -qO /tmp/dedicated.zip
+
+RUN unzip -q /tmp/dedicated.zip -d ./ \
+    && rm /tmp/dedicated.zip \
+    && rm -rf ./*.bat ./*.exe ./*.html ./RemoteControlExamples \ 
+    && chmod +x ./ManiaPlanetServer \
+    && mkdir -p ./GameData \
+    && mkdir -p ./UserData \
+    && mkdir -p ./UserData/Config \
+    && mkdir -p ./UserData/Packs \
+    && mkdir -p ./UserData/Maps \
+    && mkdir -p ./UserData/Maps/MatchSettings
+
+# Install the dedicated configuration file(s).
+COPY config.default.xml $TEMPLATE_DIR/config.txt
+COPY matchsettings.default.xml $TEMPLATE_DIR/matchsettings.txt
+#COPY stadium_map.Map.gbx $TEMPLATE_DIR/stadium_map.Map.gbx
+COPY EliteMaps/ $TEMPLATE_DIR/Maps/
+
+# Install run script.
+COPY entrypoint.sh ./start.sh
+RUN chmod +x ./start.sh
+RUN chown -R maniaplanet:maniaplanet $PROJECT_ROOT
+RUN chown -R maniaplanet:maniaplanet $TEMPLATE_DIR
+
+USER maniaplanet
+
+# Expose ports.
+EXPOSE 2350 2350/udp 3450 3450/udp 5000
+
+CMD [ "./start.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# We are required to get the public ip if we don't have it in our env currently.
+
+if [ "$FORCE_IP_ADDRESS" = "" ]
+then
+    FORCE_IP_ADDRESS=`wget -4 -qO- http://ifconfig.co`
+fi
+
+if [ "$FORCE_IP_PORT" = "" ]
+then
+    FORCE_IP_PORT=${PORT-2350}
+fi
+
+echo "=> Going to run on forced IP: ${FORCE_IP_ADDRESS} and port: ${FORCE_IP_PORT}"
+# Make sure we use defaults everywhere (DO NOT EDIT! Set desired settings in dedicated_vars.env instead).
+: ${TITLE:="SMStormElite@nadeolabs"}
+: ${TITLE_PACK_URL:="https://maniaplanet.com/ingame/public/titles/download/SMStormElite@nadeolabs.Title.Pack.gbx"}
+: ${TITLE_PACK_FILE:=""}
+: ${DEDICATED_CFG:="config.txt"}
+: ${MATCH_SETTINGS:="MatchSettings/default.txt"}
+: ${SERVER_NAME:="My Test Server"}
+
+# Copy the configuration files if not yet copied.
+mkdir -p ${PROJECT_ROOT}/UserData/Config
+mkdir -p ${PROJECT_ROOT}/UserData/Packs
+mkdir -p ${PROJECT_ROOT}/UserData/Maps/MatchSettings
+
+cp -r ${TEMPLATE_DIR}/Maps/ ${PROJECT_ROOT}/UserData/Maps/
+
+if [ ! -f ${PROJECT_ROOT}/UserData/Config/config.txt ]; then
+    cp ${TEMPLATE_DIR}/config.txt ${PROJECT_ROOT}/UserData/Config/config.txt
+fi
+
+if [ ! -f ${PROJECT_ROOT}/UserData/Maps/MatchSettings/default.txt ]; then
+    cp ${TEMPLATE_DIR}/matchsettings.txt ${PROJECT_ROOT}/UserData/Maps/MatchSettings/default.txt
+fi
+
+#if [ ! -f ${PROJECT_ROOT}/UserData/Maps/stadium_map.Map.gbx ]; then
+#    cp ${TEMPLATE_DIR}/stadium_map.Map.gbx ${PROJECT_ROOT}/UserData/Maps/stadium_map.Map.gbx
+#fi
+
+# Download title.
+# TODO: Check if update is required first.
+# TODO: Consider using curl instead
+echo "=> Downloading newest ${TITLE} version"
+if [ "$TITLE_PACK_FILE" = "" ]
+then
+    wget ${TITLE_PACK_URL} -qP ${PROJECT_ROOT}/UserData/Packs/
+else
+    wget ${TITLE_PACK_URL} -qO ${PROJECT_ROOT}/UserData/Packs/${TITLE_PACK_FILE}
+fi
+
+# Start dedicated.
+echo "=> Starting server, login=${LOGIN}"
+./ManiaPlanetServer $@ \
+    /nodaemon \
+    /forceip=${FORCE_IP_ADDRESS}:${FORCE_IP_PORT} \
+    /title=${TITLE} \
+    /dedicated_cfg=${DEDICATED_CFG} \
+    /game_settings=${MATCH_SETTINGS} \
+    /login=${LOGIN} \
+    /password=${PASSWORD} \
+    /servername="${SERVER_NAME}" \
